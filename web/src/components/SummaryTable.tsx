c539bae0a4888ae1866cb796f08a2cf599eb3514
Amanda Alimura <amanda.alimura@gmail.com>
2023-03-29 08:41:41 -0300

    feat: create days of the year

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,4 @@
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 import { HabitDay } from "./HabitDay"
 
 const weekDays = [
@@ -10,6 +11,9 @@ const weekDays = [
     'S',
 ] 
 
+const summaryDates = generateDatesFromYearBeginning()
+const minimumSumaryDatesSize = 18 * 7 //18 weeks
+const amountOfDaysToFill = minimumSumaryDatesSize - summaryDates.length
 export function SummaryTable() {
     return (
         <div className="w-full flex">
@@ -26,23 +30,14 @@ export function SummaryTable() {
                 })}
             </div>
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               <HabitDay />
-               
+               {summaryDates.map(date => {
+                return <HabitDay key={date.toString()}/>
+               })}
+               {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, i) => {
+                return (
+                    <div key={i} className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"/>
+                )
+               })}
             </div>
         </div>
     )
