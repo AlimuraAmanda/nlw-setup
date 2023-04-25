6d47c47c4831a320256ab8cf526354eb9dc2f480
Amanda Alimura <amanda.alimura@gmail.com>
2023-04-25 09:02:42 -0300

    feat: end of the stylization of the habit create screen

--- mobile/src/components/CheckBox.tsx
+++ mobile/src/components/CheckBox.tsx
@@ -1,15 +1,33 @@
-import { ScrollView, TouchableOpacity, Text, TextInput, View } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps, Text, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
+import colors from 'tailwindcss/colors';
 
-export function CheckBox(){
+interface Props extends TouchableOpacityProps {
+    title: string;
+    checked?: boolean;
+}
+
+export function CheckBox({title, checked = false, ...rest }: Props ){
     return (
         <TouchableOpacity
-        activeOpacity={0.7}
-        className=" flex-row mb-2 items-center"
+            activeOpacity={0.7}
+            className=" flex-row mb-2 items-center"
+            {...rest}
         >
-            <View className="h-8 bg-green-500 rounded-lg items-center justify-center">
-
+            {checked ? 
+                <View className="h-8 w-8 bg-green-500 rounded-lg items-center justify-center">
+                <Feather 
+                    name="check"
+                    size={20}
+                    color={colors.white}
+                />
             </View>
+            : 
+            <View className="h-8 w-8 bg-zinc-900 rounded-lg"/>
+            }
+            <Text className="text-white text-base ml-3">
+                {title}
+            </Text>
         </TouchableOpacity>
     )
 }
